1cfd0c1177af56901bda5f0f9b7e299cbf06b105
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-04 22:42:40 +0800

    created common.h

---
+++ src/common.h
@@ -0,0 +1,9 @@
+//gist the book said this is for the types and constants that will be used throughout the interpreter
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
