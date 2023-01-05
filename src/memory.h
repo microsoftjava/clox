b5dcc6740890f2590172e51fa3e7897c5a1ab4ca
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-05 19:52:18 +0800

    defined GROW_ARRAY() macro and declared reallocate() function in memory.h GROW_ARRAY() increases the memory allocated to a Chunk instance reallocate() handles all dynamic memory management in clox

--- src/memory.h
+++ src/memory.h
@@ -8,4 +8,12 @@
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
+//gist this increases the memory allocated to a Chunk instance
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+    sizeof(type) * (newCount))
+
+//desc this will handle all the dynamic memory management in clox
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+
 #endif
