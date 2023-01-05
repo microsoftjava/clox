1a943e57561fd3181754cdf5f14d9c29fe2a53e8
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-05 23:26:45 +0800

    implemented the disassembleInstruction() and simpleInstruction() functions mentioned in debug.h

--- src/debug.c
+++ src/debug.c
@@ -14,3 +14,29 @@ void disassembleChunk(Chunk* chunk, const char* name)
         offset = disassembleInstruction(chunk, offset);
     }
 }
+
+//gist this is the simpleInstruction() function mentioned in debug.h
+//gist this displays the name of an instruction then returns the next offset past the given offset
+static int simpleInstruction(const char* name, int offset)
+{
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+//gist this is the disassembleInstruction() function mentioned in debug.h
+//gist this displays the instruction at the given offset of the given
+//gist Chunk instance then returns the offset of the next instruction
+int disassembleInstruction(Chunk* chunk, int offset)
+{
+    printf("%04d ", offset);
+
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction)
+    {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
+    }
+}
