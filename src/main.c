8475ffa880519017206b849515d072dca1fd6a90
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-05 22:23:02 +0800

    made a sample chunk in main.c like the book told me to

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char* argv[])
 {
 	printf("Hello World!\n");
+
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+	freeChunk(&chunk);
 
 	return (0);
 }
