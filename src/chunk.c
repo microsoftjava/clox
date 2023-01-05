04e2f2711381c6cc6c9acea611a939138d54ca9b
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-05 17:52:58 +0800

    declared in chunk.h and currently implementing in chunk.c a writeChunk() function that appends a byte to the end of a Chunk instance

--- src/chunk.c
+++ src/chunk.c
@@ -5,8 +5,18 @@
 
 //gist this is the implementation of the initChunk() function mentioned in chunk.h
 //gist this initialises the members of the given Chunk instance
-void initChunk(Chunk* chunk) {
+void initChunk(Chunk* chunk)
+{
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte)
+{
+    if (chunk->capacity < chunk->count + 1)
+    {
+        /* code */
+    }
+    
 }
--- src/chunk.h
+++ src/chunk.h
@@ -5,18 +5,21 @@
 #include "common.h"
 
 //gist this is for the operation codes (opcodes)
-typedef enum {
+typedef enum
+{
     OP_RETURN,
 } OpCode;
 
 //gist this is for storing the bytecode and other stuff that the book hasn't revealed yet
 //gist this is a dynamic array
-typedef struct {
+typedef struct
+{
     int count; //* this is the number of used entries in the dynamic array
     int capacity; //* this is the size of the dynamic array
     uint8_t* code; //* this is the stuff in the dynamic array
 } Chunk;
 
-void initChunk(Chunk* chunk);
+void initChunk(Chunk* chunk); //desc this initialises a Chunk instance's members
+void writeChunk(Chunk* chunk, uint8_t byte); //desc this appends a byte to the end of a Chunk instance
 
 #endif
