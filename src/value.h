659d43365b67310d14dea321525f65b14e6b3608
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-08 13:08:28 +0800

    created value.h file and declared a Value type for double-precision floating-point numbers

---
+++ src/value.h
@@ -0,0 +1,10 @@
+//gist this is for numbers
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+//gist for now, numbers will be double-precision floating point
+typedef double Value;
+
+#endif
