dac8cf4cc0e6855b647f109a3708d5331b597fc5
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-01-13 15:50:56 +0800

    very minor change

--- src/debug.c
+++ src/debug.c
@@ -42,7 +42,7 @@ int disassembleInstruction(Chunk* chunk, int offset)
     printf("%04d ", offset);
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
     {
-        printf("  | ");
+        printf("   | ");
     }
     else
     {
