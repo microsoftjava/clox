7ae33c28cecc7b3f0477c448a63f6f7a00882a72
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-05 14:46:55 +0800

    new file vm.c new VM vm that will be the bytecode VM for the interpreter implemented initVM() and freeVM() from vm.h

---
+++ src/vm.c
@@ -0,0 +1,14 @@
+//gist this is for the virtual machine
+#include "common.h"
+#include "vm.h"
+
+//gist this is the virtual machine that will be used
+VM vm;
+
+//gist this is the implementation of initVM() from vm.h
+void initVM()
+{}
+
+//gist this is the implementation of freeVM() from vm.h
+void freeVM()
+{}
