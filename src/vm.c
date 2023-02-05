3bea58eb97385289a920ed2a73e261a1c4f5d413
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-05 21:10:16 +0800

    implemented interpreting of bytecode instructions

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,6 @@
 //gist this is for the virtual machine
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -12,6 +14,35 @@ void initVM()
 //gist this is the implementation of freeVM() from vm.h
 void freeVM()
 {}
+
+//gist this runs the bytecode
+static InterpretResult run()
+{
+    #define READ_BYTE() (*vm.ip++)
+    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;)
+    {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE())
+        {
+            case OP_CONSTANT:
+            {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN:
+            {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+    #undef READ_BYTE
+    #undef READ_CONSTANT
+}
 
 //gist this is the implementation of interpret() from vm.h
 InterpretResult interpret(Chunk* chunk)
