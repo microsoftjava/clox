fbb533f4af6fa485dd75208a816764c0dae02ef9
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-16 12:43:02 +0800

    finished chapter 15 added addition, subtraction, multiplication and division

--- src/vm.c
+++ src/vm.c
@@ -39,6 +39,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do \
+    { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
     for (;;)
     {
@@ -64,6 +71,10 @@ static InterpretResult run()
                 push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
             case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:
             {
@@ -76,6 +87,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 //gist this is the implementation of interpret() from vm.h
