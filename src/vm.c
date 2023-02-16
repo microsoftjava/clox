b84f84a45ca5c8c70a042faef76c70d421ce6664
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-16 12:11:07 +0800

    added unary negation

--- src/vm.c
+++ src/vm.c
@@ -64,6 +64,7 @@ static InterpretResult run()
                 push(constant);
                 break;
             }
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:
             {
                 printValue(pop());
