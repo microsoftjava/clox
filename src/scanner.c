a716beda00d1e8bb6ef119ffe753c385279e65a0
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-17 11:20:14 +0800

    implemented token scanning

--- src/scanner.c
+++ src/scanner.c
@@ -23,3 +23,39 @@ void initScanner(const char* source)
     scanner.current = source;
     scanner.line = 1;
 }
+
+//gist this checks the current character to see if the end of the sourcecode has been reached
+static bool isAtEnd()
+{return *scanner.current == '\0';}//* the sourcecode is now a c string so it will end with a null byte
+
+//gist this makes tokens
+static Token makeToken(TokenType type)
+{
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+//gist this makes error tokens
+static Token errorToken(const char* message)
+{
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message; //* message is a char pointer so it points to the first character in the message
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+    return token;
+}
+
+//gist this is the implementation of scanToken() from scanner.h
+Token scanToken()
+{
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    return errorToken("Unexepected character.");
+}
