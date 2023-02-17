594d5186591c96c8cb52d27e00badd7363bf6791
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-17 15:58:18 +0800

    implemented scanning of one- and two-character tokens excluding keywords

--- src/scanner.c
+++ src/scanner.c
@@ -28,6 +28,23 @@ void initScanner(const char* source)
 static bool isAtEnd()
 {return *scanner.current == '\0';}//* the sourcecode is now a c string so it will end with a null byte
 
+//gist this returns the current character and moves the scanner to the next character
+static char advance()
+{
+    scanner.current++;
+    return scanner.current[-1];//* this does the same thing as *(scanner.current - 1)
+}
+
+//gist this determines if the current character matches the expected character
+//gist and moves the scanner to the next character if they match
+static bool match(char expected)
+{
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+    scanner.current++;
+    return true;
+}
+
 //gist this makes tokens
 static Token makeToken(TokenType type)
 {
@@ -56,6 +73,35 @@ Token scanToken()
     scanner.start = scanner.current;
 
     if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    char c = advance();
+
+    switch (c)
+    {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(TOKEN_LEFT_BRACE);
+        case '}': return makeToken(TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case '!':
+            return makeToken(
+                match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=':
+            return makeToken(
+                match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<':
+            return makeToken(
+                match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>':
+            return makeToken(
+                match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
 
     return errorToken("Unexepected character.");
 }
