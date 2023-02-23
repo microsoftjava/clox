754340079181604932173b3fd7e6bed31d6dab39
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-23 12:55:28 +0800

    finished chapter 16 the scanner can scan identifiers and keywords now

--- src/scanner.c
+++ src/scanner.c
@@ -25,6 +25,14 @@ void initScanner(const char* source)
 }
 
 //gist this is self-explanatory
+static bool isAlpha(char c)
+{
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+}
+
+//gist this is self-explanatory
 static bool isDigit(char c)
 {return c >= '0' && c <= '9';}
 
@@ -112,6 +120,67 @@ static void skipWhitespace()
     }
 }
 
+//gist this determines if the given lexeme is a keyword or an identifier
+static TokenType checkKeyword(int start, int length, const char* rest, TokenType type)
+{
+    if (scanner.current - scanner.start == start + length &&
+        memcmp(scanner.start + start, rest, length) == 0)
+    {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+//gist this identifies the token type an identifier or keyword lexeme should have
+static TokenType identifierType()
+{
+    switch (scanner.start[0])
+    {
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if (scanner.current - scanner.start > 1)
+            {
+                switch (scanner.start[1])
+                {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if (scanner.current - scanner.start > 1)
+            {
+                switch (scanner.start[1])
+                {
+                    case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+//gist this makes identifier and keyword tokens
+static Token identifier()
+{
+    while (isAlpha(peek()) || isDigit(peek())) advance();
+    return makeToken(identifierType());
+}
+
 //gist this makes number tokens
 static Token number()
 {
@@ -153,6 +222,7 @@ Token scanToken()
     if (isAtEnd()) return makeToken(TOKEN_EOF);
 
     char c = advance();
+    if (isAlpha(c)) return identifier();
     if (isDigit(c)) return number();
 
     switch (c)
