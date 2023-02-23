9676829722447b7dd8cd7981b1d08a113e26d27d
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-23 11:10:54 +0800

    the scanner can scan string literals and numbers now

--- src/scanner.c
+++ src/scanner.c
@@ -24,6 +24,10 @@ void initScanner(const char* source)
     scanner.line = 1;
 }
 
+//gist this is self-explanatory
+static bool isDigit(char c)
+{return c >= '0' && c <= '9';}
+
 //gist this checks the current character to see if the end of the sourcecode has been reached
 static bool isAtEnd()
 {return *scanner.current == '\0';}//* the sourcecode is now a c string so it will end with a null byte
@@ -108,6 +112,38 @@ static void skipWhitespace()
     }
 }
 
+//gist this makes number tokens
+static Token number()
+{
+    while (isDigit(peek())) advance();
+
+    //* check for decimals
+    if (peek() == '.' && isDigit(peekNext()))
+    {
+        advance(); //* move past the .
+
+        while (isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+//gist this makes string literal tokens
+static Token string()
+{
+    while (peek() != '\'' && !isAtEnd())
+    {
+        if (peek() == '\n') scanner.line++;
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    //* the current character is now the ' that ends the string literal so we move past it
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
 //gist this is the implementation of scanToken() from scanner.h
 Token scanToken()
 {
@@ -117,6 +153,7 @@ Token scanToken()
     if (isAtEnd()) return makeToken(TOKEN_EOF);
 
     char c = advance();
+    if (isDigit(c)) return number();
 
     switch (c)
     {
@@ -143,6 +180,7 @@ Token scanToken()
         case '>':
             return makeToken(
                 match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        case '\'': return string();
     }
 
     return errorToken("Unexepected character.");
