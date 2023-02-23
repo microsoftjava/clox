7dddbb3cca0fc4235dda6e67bc49ea2543a43089
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-23 19:32:11 +0800

    the parser can handle syntax errors now

--- src/compiler.c
+++ src/compiler.c
@@ -1,5 +1,6 @@
 //gist this is for the bytecode compiler
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -10,10 +11,39 @@ typedef struct
 {
     Token current;
     Token previous;
+    bool hadError;
+    bool panicMode;
 } Parser;
 
 //gist this is the parser
 Parser parser;
+
+//gist this reports an error at the given token
+static void errorAt(Token* token, const char* message)
+{
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        //* nothing happens here
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+//gist this reports an error at the previous token
+static void error(const char* message)
+{errorAt(&parser.previous, message);}
+
+//gist this reports an error at the current token
+static void errorAtCurrent(const char* message)
+{errorAt(&parser.current, message);}
 
 //gist this advances through the generated tokens
 static void advance()
@@ -30,11 +60,28 @@ static void advance()
     }
 }
 
+//gist this reports an error if the type of the current token is not the expected type
+static void consume(TokenType type, const char* message)
+{
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
 //gist this is the implementation of compile() from compiler.h
 bool compile(const char* source, Chunk* chunk)
 {
     initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
