c306891495597063e6d6f502d6ae9f5985b75364
microsoftjava <121544303+microsoftjava@users.noreply.github.com>
2023-02-23 17:41:44 +0800

    made the parser

--- src/compiler.c
+++ src/compiler.c
@@ -5,6 +5,31 @@
 #include "compiler.h"
 #include "scanner.h"
 
+//gist this is for the parser
+typedef struct
+{
+    Token current;
+    Token previous;
+} Parser;
+
+//gist this is the parser
+Parser parser;
+
+//gist this advances through the generated tokens
+static void advance()
+{
+    parser.previous = parser.current;
+
+    for (;;)
+    {
+        parser.current = scanToken();
+        //* if there is an error token, the parser advances until it encounters a non-error token
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
 //gist this is the implementation of compile() from compiler.h
 bool compile(const char* source, Chunk* chunk)
 {
